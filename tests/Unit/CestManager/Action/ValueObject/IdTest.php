5953d9de8dd67a2bf6b60b1882de801ecb1eded3
Gerald Baumeister <gerald.b@whosonlocation.com>
2021-03-02 09:13:45 +1300

    Tests for ActionId

---
+++ tests/Unit/CestManager/Action/ValueObject/IdTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+namespace App\Tests\Unit\CestManager\Action\ValueObject;
+
+use ApiPlatform\Core\Exception\InvalidIdentifierException;
+use App\CestManager\Action\ValueObject\Id;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\CestManager\Exception\ValueObject\InvalidIdentifierStringException;
+
+/**
+ * 
+ * @package Fantestic
+ * @author Gerald Baumeister <gerald@fantestic.io>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+final class IdTest extends KernelTestCase
+{
+    public function testThrowsInvalidIdentifierString() :void
+    {
+        $this->expectException(InvalidIdentifierException::class);
+        Id::fromString('A invalid identifier');
+    }
+
+    public function testDoesNotModifiyAValidIdentifierString() :void
+    {
+        $idString = 'aValidActionName';
+        $id = Id::fromString($idString);
+        $this->assertSame($idString, $id);
+    }
+}
